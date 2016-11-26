c54af0f767d6ed791953254b954a6efed9389ef1
David Clark <plotinussmith@gmail.com>
2016-11-26 17:29:07 -0600

    Finishing presentation.

--- BasicComputation.groovy
+++ BasicComputation.groovy
@@ -1,26 +1,49 @@
-import java.util.concurrent.ThreadLocalRandom;
-import groovyx.gpars.dataflow.DataflowVariable;
+import groovyx.gpars.dataflow.*;
 import static Pools.*;
+import java.util.concurrent.ThreadLocalRandom;
 
-final DataflowVariable m = new DataflowVariable();
-final DataflowVariable a = new DataflowVariable();
+//We want to model an equation with lots of dependencies
+//y = a*x2 + b*x * c
+//x = m * r
+//a = q * 17
+//b = r / s
+//c = 5 + s
+//what are the things we need to know?: s, q, m, r
 
-COMPUTE_GROUP.task { ->
-    sleep(1000L);
-    m << 10;
+final DataflowBroadcast s = new DataflowBroadcast();
+final DataflowBroadcast q = new DataflowBroadcast();
+final DataflowBroadcast m = new DataflowBroadcast();
+final DataflowBroadcast r = new DataflowBroadcast();
+
+final DataflowReadChannel s2cRead = s.createReadChannel();
+def c = { -> return 5 + s2cRead.val; }
+
+final DataflowReadChannel r2bRead = r.createReadChannel();
+final DataflowReadChannel s2bRead = s.createReadChannel();
+def b = { -> return r2bRead.val / s2bRead.val; }
+
+final DataflowReadChannel q2aRead = q.createReadChannel();
+def a = { -> return q2aRead.val * 17; }
+
+final DataflowReadChannel m2xRead = m.createReadChannel();
+final DataflowReadChannel r2xRead = r.createReadChannel();
+def x = { -> return m2xRead.val * r2xRead.val; }
+
+def y = { ->
+    def myX = x();
+    return (a() * myX * myX) + (b() * myX) + c();
 }
 
-COMPUTE_GROUP.task { ->
-    sleep(1000L);
-    a << 15;
+def rand = { max -> ThreadLocalRandom.current().nextLong(max); }
+
+COMPUTE_GROUP.task { sleep(rand(1000L)); s << 5; }
+COMPUTE_GROUP.task { sleep(rand(1500L)); q << 10; }
+COMPUTE_GROUP.task { sleep(rand(700L)); m << 15; }
+COMPUTE_GROUP.task { sleep(rand(100L)); r << 20; }
+
+long nanos = timeIt {
+    println("y is: ${y()}")
 }
 
-def F = m.val * a.val;
+println("Total time to compute: ${nanos / 1_000_000} ms")
 
-println "Force is ${F}"
-
-//can also so this directly
-F = (COMPUTE_GROUP.task { -> sleep(1000L); return 10; }.val *
-     COMPUTE_GROUP.task { -> sleep(sleep(1000L); return 15; }.val);
-
-println "Force is still ${F}"
